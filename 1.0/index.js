5c6b86fe3f760f4c1dbf271148ddc697cd8502db
乔福 <qiaofu@taobao.com>
2014-05-15 20:50:39 +0800

    放大镜中小图片放大到和大图片一样

--- 1.0/index.js
+++ 1.0/index.js
@@ -452,6 +452,12 @@ KISSY.add(function (S, Node, Overlay, Base, undefined) {
             src: image.attr('src'),
             style: ABSOLUTE_STYLE
         })).prependTo(contentEl, undefined);
+
+        // copy的小图片宽度设定和大图片一样
+        self.bigImageCopy.css({
+            "width" : self.get('bigImageWidth'),
+            "height" : self.get('bigImageHeight')
+        });
     }
 
     function imageZoomBindUI(self) {
